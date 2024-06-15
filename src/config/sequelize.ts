444dbfbf2e0c90ed7eb4e82077bb6913f78769dd
Rodrigo Matos <rodrigomoreira@talkfan.com>
2024-06-15 17:56:50 -0300

    feat: add projects exclude

--- src/config/sequelize.ts
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('mydatabase', 'myuser', 'mypassword', {
-    host: 'db',
-    dialect: 'postgres',
-    logging: false,
-});
-
-export default sequelize;
