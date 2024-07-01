510b6a5710cb1fac034f7abb8218b058e30f9657
kemily.rosa <kemily.rosa@becomex.com.br>
2024-07-01 20:53:59 -0300

    feat: add swagger

--- src/app/users/users.controller.ts
+++ src/app/users/users.controller.ts
@@ -1,41 +1,48 @@
 import {
-  Controller,
-  Get,
-  Post,
   Body,
-  Param,
-  Put,
+  Controller,
   Delete,
+  Get,
+  Param,
+  Post,
+  Put,
 } from "@nestjs/common";
-import { UsersService } from "./users.service";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
+import { UsersService } from "./users.service";
 
 @Controller("users")
+@ApiTags("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiOperation({ summary: "Create a new user" })
   async create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ApiOperation({ summary: "List all users" })
   async findAll() {
     return this.usersService.findAll();
   }
 
   @Get(":id")
+  @ApiOperation({ summary: "Find a user by ID" })
   async findOne(@Param("id") id: string) {
     return this.usersService.findOne(+id);
   }
 
   @Put(":id")
+  @ApiOperation({ summary: "Update a user by ID" })
   async update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(":id")
+  @ApiOperation({ summary: "Remove a user by ID" })
   async remove(@Param("id") id: string) {
     return this.usersService.remove(+id);
   }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerDocumentOptions, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app/app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.startAllMicroservices();
+
+  const config = new DocumentBuilder()
+    .setTitle('Sistema de Reserva de Salas')
+    .setDescription('API para comunicação com o sistema de reserva de salas.')
+    .setVersion('1.0')
+    .build();
+
+  const options: SwaggerDocumentOptions =  {
+    operationIdFactory: (
+      controllerKey: string,
+      methodKey: string
+    ) => methodKey
+  };
+  
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3005);
 }
 bootstrap();
